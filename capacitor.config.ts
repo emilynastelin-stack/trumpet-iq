fe6c9d3ed5972198da463ddde020753403719414
Emily <nastelin@usc.edu>
2025-11-04 12:02:31 -0800

    Update appId and app name to com.emilynastelin.trumpetlab

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.user944848.trumpetiq',
-  appName: 'Trumpet IQ',
+  appId: 'com.emilynastelin.trumpetlab',
+  appName: 'Trumpet Lab',
   webDir: 'dist'
 };
 
